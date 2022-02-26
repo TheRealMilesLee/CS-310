9c5a4d26d81d07f64119d311fb85a38d02cf8586
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-02-26 13:25:55 -0600

    new file:   Week5-HashMap/Hasing.cpp Update the initial template

---
+++ Week5-HashMap/Hasing.cpp
@@ -0,0 +1,34 @@
+/**
+ * Program explanation here
+ * @author Your Name Here
+ * @version Date Last Modified Here
+ */
+
+#include <cstdint>
+#include <iostream>
+using namespace std;
+
+/**
+ * documentation here
+ */
+size_t hashx(const string& key, size_t table_size);
+
+int main()
+{
+  string word;
+  while (getline(cin, word))
+  {
+    // do something with the word
+  }
+  return 0;
+}
+
+size_t hashx(const string& key, size_t table_size)
+{
+  size_t hash_value = 1;
+  for (auto character : key)
+  {
+    hash_value = 43 * hash_value + static_cast<size_t>(character);
+  }
+  return hash_value % table_size;
+}
