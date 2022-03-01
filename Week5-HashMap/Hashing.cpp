82478cf0ad7e89de97898217080d8822de3a774c
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-03-01 14:38:47 -0600

    Finished the table size

--- Week5-HashMap/Hashing.cpp
+++ Week5-HashMap/Hashing.cpp
@@ -1,9 +1,12 @@
 /**
- * Program explanation here
- * @author Your Name Here
- * @version Date Last Modified Here
+ * @file Hashing.cpp
+ * @author Hengyi Li
+ * @brief This file is to count the collisions of words in a hash table using
+ * hashx function which is to get the index in the hash table.
+ * @version 0.2.3.1 alpha
+ * @date 2022-03-01
+ * @copyright Copyright (c) 2022. Hengyi Li, All rights reserved.
  */
-
 #include <cstdint>
 #include <iostream>
 #include <string>
@@ -12,7 +15,10 @@
 using namespace std;
 
 /**
- * documentation here
+ * @brief  This function is to get the hash index for the input words
+ * @param key is the words from input
+ * @param table_size is the size of the hash table
+ * @return size_t is the index which the hash key corresponds to
  */
 size_t hashx(const string& key, size_t table_size);
 
@@ -20,7 +26,7 @@ int main()
 {
   string word;
   unsigned count = 0;
-  size_t table_size = 102401;
+  size_t table_size = 102409;
   vector<size_t> all_word_index;
   while (getline(cin, word))
   {
@@ -28,6 +34,7 @@ int main()
     size_t hash_index = hashx(word, table_size);
     all_word_index.push_back(hash_index);
   }
+  // Sort the vector for easy search
   sort(all_word_index.begin(), all_word_index.end());
   for (size_t index = 0; index < all_word_index.size() - 1; index++)
   {
@@ -37,7 +44,7 @@ int main()
     }
   }
   cout << count << endl;
-    return 0;
+  return 0;
 }
 
 size_t hashx(const string& key, size_t table_size)
